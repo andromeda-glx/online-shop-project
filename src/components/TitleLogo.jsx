bc0286d8d2e701c8c6353d2d196ce03ab716604a
Faran <faran.7j99@gmail.com>
2025-02-18 15:12:20 +0330

    style logo

--- src/components/TitleLogo.jsx
+++ src/components/TitleLogo.jsx
@@ -1,8 +1,14 @@
+import img from "../assets/logos/logo.png"
+
 export default function TitleLogo() {
     return (
-        <div>
-            <div className=""></div>
-            <span>Minimal Shopping</span>
+        <div className="flex items-center">
+            <div className="">
+                <img src={img} alt="" />
+            </div>
+            <span className="text-2xl text-main-theme font-semibold">
+                Minimal <span className="text-secondary-theme">Shop</span>ping
+            </span>
         </div>
     )
 }
