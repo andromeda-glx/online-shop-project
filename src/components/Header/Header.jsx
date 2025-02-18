4d96d7042e26a47fd396246eb9ab16f2e503b034
Faran <faran.7j99@gmail.com>
2025-02-18 23:43:01 +0330

    fix spaing in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ import TitleLogo from "../TitleLogo";
 export default function Header() {
     return (
         <header className="px-[67px] bg-gray-100 flex justify-between items-center max-w-[1500px] mx-auto py-3">
-            <div className="flex items-center gap-x-6">
+            <div className="flex items-center gap-x-3">
                 <TitleLogo />
                 <Navbar />
             </div>
