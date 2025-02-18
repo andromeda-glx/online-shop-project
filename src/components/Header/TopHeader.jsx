b15bc56f4ee9e44a2b2b7847a508e8a31b9943fe
Faran <faran.7j99@gmail.com>
2025-02-18 21:42:29 +0330

    make top header responsive

--- src/components/Header/TopHeader.jsx
+++ src/components/Header/TopHeader.jsx
@@ -4,8 +4,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 /* eslint-disable react/prop-types */
 export default function TopHeader({ phoneNumber, offer }) {
     return (
-        <header className="bg-main-theme text-white grid grid-cols-3 items-center py-[20px] w-[100%] px-10">
-            <span className="flex items-center gap-x-2">
+        <header className="bg-main-theme text-white grid grid-cols-1 grid-rows-2 gap-y-2 lg:grid-cols-3 lg:grid-rows-1 items-center py-[20px] w-[100%] px-10 justify-center">
+            <span className="flex items-center gap-x-2 justify-center lg:justify-start">
                 <FontAwesomeIcon icon={faPhone} />
                 {phoneNumber}
             </span>
