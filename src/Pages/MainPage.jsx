e0a54b304fa499eec956800486f4668e45fb3081
Faran <faran.7j99@gmail.com>
2025-04-07 09:47:53 +0330

    write fetch requests

--- src/Pages/MainPage.jsx
+++ src/Pages/MainPage.jsx
@@ -1,10 +1,18 @@
 import Button from "../components/Button";
 import Products from "../components/Products";
 import featuredImage from "../assets/images/offer-feature-banner.jpg"
+import { useQuery } from "@tanstack/react-query";
+import { getProducts } from "../services/products";
 
 export default function MainPage() {
-    
+    const productsQuery = useQuery({
+        queryKey: ['/products'],
+        queryFn: () => getProducts()
+    })
 
+    const products = productsQuery.data?.flat();
+    console.log(products);
+    
     return (
         <>
             <div style={{ backgroundImage: `url(${featuredImage})` }} className={`w-[100%] h-[360px] bg-no-repeat flex justify-center items-center bg-center`}>
--- src/services/products.js
+++ src/services/products.js
@@ -3,13 +3,15 @@ import axios from "axios";
 const productCategories = ["mens-shirts", "mens-shoes", "mens-watches", "sports-accessories", "tops", "womens-bags", "womens-dresses", "womens-jewellery", "womens-shoes", "womens-watches"];
 
 export async function getProducts(){
-    const data = [];
-    let products = null;
+    const urls = productCategories.map(category => {
+        return `https://dummyjson.com/products/category/${category}`;
+    })
 
-    productCategories.forEach(async (category) => {
-        products = await axios.get(`https://dummyjson.com/products/${category}`);
-        data.push(products);
-    });
+    const responses = await Promise.all(
+        urls.map(url => axios.get(url))
+    );
+
+    const data = responses.map(res => res.data.products);
 
     return data;
 }
