129585df74ebfe3b4974df749cd25a449c28393f
Faran <faran.7j99@gmail.com>
2025-04-08 08:02:37 +0330

    make size buttons component

---
+++ src/components/SizeButtons.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import SizeButton from "./SizeButton";
+
+export default function SizeButtons() {
+    const [sizeChecked, setSizeChecked] = useState('s');
+
+    return (
+        <div className="flex gap-x-3 mt-5">
+            <SizeButton
+                id="s"
+                name="s"
+                sizeChecked={sizeChecked}
+                setSizeChecked={setSizeChecked}
+                text="S"
+            />
+            <SizeButton
+                id="m"
+                name="m"
+                sizeChecked={sizeChecked}
+                setSizeChecked={setSizeChecked}
+                text="M"
+            />
+            <SizeButton
+                id="l"
+                name="l"
+                sizeChecked={sizeChecked}
+                setSizeChecked={setSizeChecked}
+                text="L"
+            />
+            <SizeButton
+                id="xl"
+                name="xl"
+                sizeChecked={sizeChecked}
+                setSizeChecked={setSizeChecked}
+                text="XL"
+            />
+            <SizeButton
+                id="xxl"
+                name="xxl"
+                sizeChecked={sizeChecked}
+                text="XXL"
+                setSizeChecked={setSizeChecked}
+            />
+        </div>
+    )
+}
