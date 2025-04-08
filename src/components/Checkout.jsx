ae18c8993dab8aa4fa7d246e55f19da5b766e8ca
Faran <faran.7j99@gmail.com>
2025-04-08 12:16:46 +0330

    fix checkout style

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -20,12 +20,12 @@ export default function Checkout() {
     return (
         <div className="grid grid-cols-5 grid-rows-5 gap-5 max-h-[100%]">
             <div className="border border-gray-500 col-span-3 row-span-3 p-5 rounded-lg overflow-y-auto h-[250px]">
-                <h2>Cart Details</h2>
-                { productsByIdQuery.isLoading ? <Spinner /> :
+                <h2 className="text-xl">Cart Details</h2>
+                {productsByIdQuery.isLoading ? <Spinner /> :
                     <div className="h-[100%] mt-2">
                         <ul className="flex flex-col gap-y-2">
                             {products.map(product => (
-                                <li key={product.id} className="border-t p-2">
+                                <li key={product.id}>
                                     <CheckoutProduct product={product} />
                                 </li>
                             ))}
@@ -33,11 +33,22 @@ export default function Checkout() {
                     </div>
                 }
             </div>
-            <div className="border border-gray-500 row-start-4 col-span-3 row-span-2 p-5 rounded-lg h-[50%%]">
-                <h2>Delivery Information</h2>
+            <div className="border border-gray-500 row-start-4 col-span-3 row-span-2 p-5 rounded-lg">
+                <h2 className="text-xl mb-2">
+                    Delivery Information
+                </h2>
+                <address className="text-gray-500">
+                    John Smith<br />
+                    New Zealand<br />
+                    <br />
+                    CrossRoad - Po25698<br />
+                    United States
+                </address>
             </div>
             <div className="border border-gray-500 col-span-2 row-span-5 p-5 rounded-lg">
-                Order Summary
+                <h2 className="text-xl">
+                    Order Summary
+                </h2>
             </div>
         </div>
     )
--- src/components/CheckoutProduct.jsx
+++ src/components/CheckoutProduct.jsx
@@ -5,19 +5,19 @@ import Rating from "./Rating";
 export default function CheckoutProduct({ product }) {
     const { id, title, thumbnail, price, rating, description } = product;
     return (
-        <div className="grid grid-cols-[50px_1fr_50px]">
+        <div className="grid grid-cols-[150px_1fr] items-center">
             <div className="">
                 <img src={thumbnail} alt="" />
                 <span></span> {/* amount */}
             </div>
-            <div className="flex justify-between">
+            <div className="flex justify-between gap-x-2">
                 <div>
                     <h3 className="clamp-text w-[60%] text-lg">{title}</h3>
                     <p className="clamp-text text-sm text-gray-500">{description}</p>
                     <Rating rating={rating} />
                 </div>
                 <div>
-                    <p>{formatPrice(price)}</p>
+                    <p className="font-semibold">{formatPrice(price)}</p>
                 </div>
             </div>
         </div>
