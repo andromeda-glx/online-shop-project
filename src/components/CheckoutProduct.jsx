69737bbf40e3f3197897b85f93f4cb959adf3302
Faran <faran.7j99@gmail.com>
2025-04-09 08:09:56 +0330

    make footer features dynamic

--- src/components/CheckoutProduct.jsx
+++ src/components/CheckoutProduct.jsx
@@ -6,9 +6,11 @@ export default function CheckoutProduct({ product }) {
     const { id, title, thumbnail, price, rating, description, quantity } = product;
     return (
         <div className="grid grid-cols-[150px_1fr] items-center">
-            <div className="">
+            <div className="relative">
                 <img src={thumbnail} alt="" />
-                <span>{quantity}</span>
+                <span className="absolute bg-secondary-theme w-9 h-9 bottom-5 right-5 rounded-full flex justify-center items-center text-white text-lg">
+                    {quantity}
+                </span>
             </div>
             <div className="flex justify-between gap-x-2">
                 <div>
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,18 +2,23 @@ import { faHeart } from "@fortawesome/free-regular-svg-icons";
 import { faShoppingBag } from "@fortawesome/free-solid-svg-icons";
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons/faShoppingCart";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import useCart from "../../stores/cart";
+import useWishList from "../../stores/wishlist";
 
 export default function Footer() {
+    const numberOfItems = useCart(state => state.cartItems.reduce((total, item) => total + item.quantity, 0));
+    const numberOfFavorites = useWishList(state => state.itemIds.length);
+
     return (
         <footer className="bg-main-theme text-white px-5 flex justify-between items-center py-4">
             <div className="flex sm:flex-row flex-col gap-x-5 items-center">
                 <div className="flex gap-x-2 items-center">
                     <FontAwesomeIcon icon={faShoppingBag} />
-                    <span>5 Items added to Cart</span>
+                    <span>{numberOfItems} Items added to Cart</span>
                 </div>
                 <div className="flex gap-x-2 items-center">
                     <FontAwesomeIcon icon={faHeart} />
-                    <span>2 Items in the Wish list</span>
+                    <span>{numberOfFavorites} Items in the Wish list</span>
                 </div>
             </div>
             <div className="">
