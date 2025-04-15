6dcbce082bcb697088ff0743e41d694c7523b871
Faran <faran.7j99@gmail.com>
2025-04-15 10:08:08 +0330

    refactor: convert services/products to ts

--- src/services/products.ts
+++ src/services/products.ts
@@ -3,12 +3,13 @@ import axios from "axios";
 const productCategories = ["mens-shirts", "mens-shoes", "mens-watches", "sports-accessories", "tops", "womens-bags", "womens-dresses", "womens-jewellery", "womens-shoes", "womens-watches"];
 
 export async function getProducts(){
-    const urls = productCategories.map(category => {
+    const urls = productCategories.map((category: string) => {
         return `https://dummyjson.com/products/category/${category}`;
     })
 
+    /* TODO: Infer the type */
     const responses = await Promise.all(
-        urls.map(url => axios.get(url))
+        urls.map((url: string) => axios.get(url))
     );
 
     const data = responses.map(res => res.data.products);
@@ -16,9 +17,10 @@ export async function getProducts(){
     return data;
 }
 
-export async function getProductsById(ids){
+export async function getProductsById(ids: number[]){
+    /* TODO: Infer the type */
     const responses = await Promise.all(
-        ids.map(id => getProduct(id))
+        ids.map((id: number) => getProduct(id))
     );
 
     const data = responses.map(res => res.data);
@@ -26,7 +28,8 @@ export async function getProductsById(ids){
     return data;    
 }
 
-export async function getProduct(productId){
+export async function getProduct(productId: number){
+    /* TODO: Infer the type */
     const data = axios.get(`https://dummyjson.com/products/${productId}`);
     return data;
 }
