d639fe8c538a3f5e915e72b212aba29003bd9b6f
Faran <faran.7j99@gmail.com>
2025-04-15 08:38:28 +0330

    refactor: convert format-price util to ts

--- src/utils/format-price.js
+++
@@ -1,9 +0,0 @@
-export default function formatPrice(price){
-    const formattedPrice = new Intl.NumberFormat('en-US', {
-        style: 'currency',
-        currency: 'USD',
-        minimumFractionDigits: 2
-    }).format(price);
-
-    return formattedPrice;
-}
