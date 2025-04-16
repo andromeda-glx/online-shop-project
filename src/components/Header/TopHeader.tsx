c7a2b0fa2232ecd51aec6ea35d6015dcc76f7fcd
Faran <faran.7j99@gmail.com>
2025-04-16 16:05:59 +0330

    refactor: migrate TopHeader.jsx to TopHeader.tsx

--- src/components/Header/TopHeader.tsx
+++ src/components/Header/TopHeader.tsx
@@ -1,8 +1,12 @@
 import { faPhone } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-/* eslint-disable react/prop-types */
-export default function TopHeader({ phoneNumber, offer }) {
+type TopHeaderProps = {
+    phoneNumber: string;
+    offer: string;
+}
+
+const TopHeader = ({ phoneNumber, offer }: TopHeaderProps) => {
     return (
         <header className="bg-main-theme text-white grid grid-cols-1 grid-rows-2 gap-y-2 lg:grid-cols-3 lg:grid-rows-1 items-center py-[5px] w-[100%] px-10 justify-center">
             <span className="flex items-center gap-x-2 justify-center lg:justify-start">
@@ -15,3 +19,5 @@ export default function TopHeader({ phoneNumber, offer }) {
         </header>
     )
 }
+
+export default TopHeader;
