57e2ad4748d31d2fbf2048ebe9da2f8d08bcf07b
Faran <faran.7j99@gmail.com>
2025-04-16 13:23:24 +0330

    refactor: migrate Rating.jsx to Rating.tsx

---
+++ src/components/Common/Rating.tsx
@@ -0,0 +1,30 @@
+/* eslint-disable react/prop-types */
+import { faStar as faStarFull } from "@fortawesome/free-solid-svg-icons";
+import { faStar as faStarEmpty } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+type RatingProps = {
+  rating: number;
+};
+
+const Rating = ({ rating }: RatingProps) => {
+  rating = Math.round(rating);
+
+  function generateStars() {
+    const stars = new Array(5);
+
+    for (let i = 0; i < 5; i++) {
+      if (i < rating) {
+        stars.push(<FontAwesomeIcon key={i} icon={faStarFull} />);
+      } else {
+        stars.push(<FontAwesomeIcon key={i} icon={faStarEmpty} />);
+      }
+    }
+
+    return stars;
+  }
+
+  return <div className="text-main-theme">{generateStars()}</div>;
+};
+
+export default Rating;
