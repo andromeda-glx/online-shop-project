b3eb175a5ca01b365afe3faaa9e73000df934d9a
Faran <faran.7j99@gmail.com>
2025-04-16 13:30:47 +0330

    refactor: migrate Spinner.jsx to Spinner.tsx

--- src/components/Common/Spinner.tsx
+++ src/components/Common/Spinner.tsx
@@ -1,6 +1,8 @@
-export default function Spinner() {
+const Spinner = () => {
     return (
         <div className="mx-auto w-10 h-10 rounded-full border-4 border-gray-200 border-r-secondary-theme animate-spin">
         </div>
     )
 }
+
+export default Spinner;
