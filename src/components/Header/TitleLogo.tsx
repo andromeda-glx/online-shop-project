79bc1123ab0af105196f591799f3c51a56747338
Faran <faran.7j99@gmail.com>
2025-04-16 16:04:04 +0330

    fix: update logo image source path in TitleLogo component

--- src/components/Header/TitleLogo.tsx
+++ src/components/Header/TitleLogo.tsx
@@ -1,15 +1,19 @@
 const TitleLogo = () => {
-    return (
-        <div className="flex items-center">
-            <div className="w-[55px]">
-                <img className="w-[100%]" src={"/logo.png"} alt="MinimalMarket logo" />
-            </div>
-            <span className="text-2xl text-main-theme font-semibold hidden lg:block">
-                Minimal 
-                <span className="text-secondary-theme">Shop</span>ping
-            </span>
-        </div>
-    )
-}
+  return (
+    <div className="flex items-center">
+      <div className="w-[55px]">
+        <img
+          className="w-[100%]"
+          src="/online-shop-project/images/logo.png"
+          alt="MinimalMarket logo"
+        />
+      </div>
+      <span className="text-2xl text-main-theme font-semibold hidden lg:block">
+        Minimal
+        <span className="text-secondary-theme">Shop</span>ping
+      </span>
+    </div>
+  );
+};
 
 export default TitleLogo;
