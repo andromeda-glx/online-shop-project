d3c9c159145dde06be131414eb0b3f050cc3a0e7
Faran <faran.7j99@gmail.com>
2025-04-17 07:54:50 +0330

    refactor: improve type inference and formatting in products service

--- src/services/products.ts
+++ src/services/products.ts
@@ -1,34 +1,50 @@
 import axios from "axios";
+import { Product } from "../types/product";
 
-const productCategories = ["mens-shirts", "mens-shoes", "mens-watches", "sports-accessories", "tops", "womens-bags", "womens-dresses", "womens-jewellery", "womens-shoes", "womens-watches"];
+const productCategories = [
+    "mens-shirts",
+    "mens-shoes",
+    "mens-watches",
+    "sports-accessories",
+    "tops",
+    "womens-bags",
+    "womens-dresses",
+    "womens-jewellery",
+    "womens-shoes",
+    "womens-watches",
+];
 
-export async function getProducts(){
+type ProductResponse = {
+    products: Product[];
+};
+
+/* TODO: Infer the type */
+export async function getProducts(): Promise<Product[][]> {
     const urls = productCategories.map((category: string) => {
         return `https://dummyjson.com/products/category/${category}`;
-    })
+    });
 
-    /* TODO: Infer the type */
     const responses = await Promise.all(
-        urls.map((url: string) => axios.get(url))
+        urls.map((url: string) => axios.get<ProductResponse>(url))
     );
 
-    const data = responses.map(res => res.data.products);
+    const data = responses.map((res) => res.data.products);
 
     return data;
 }
 
-export async function getProductsById(ids: number[]){
+export async function getProductsById(ids: number[]) {
     /* TODO: Infer the type */
     const responses = await Promise.all(
         ids.map((id: number) => getProduct(id))
     );
 
-    const data = responses.map(res => res.data);
+    const data = responses.map((res) => res.data);
 
-    return data;    
+    return data;
 }
 
-export async function getProduct(productId: number){
+export async function getProduct(productId: number) {
     /* TODO: Infer the type */
     const data = axios.get(`https://dummyjson.com/products/${productId}`);
     return data;
