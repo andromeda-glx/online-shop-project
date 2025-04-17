fc93059d96324bde9b9bcbc60d65adc6bc1e8b3e
Faran <faran.7j99@gmail.com>
2025-04-17 08:20:37 +0330

    refactor: migrate main.jsx to main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,30 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            staleTime: 30000,
+            gcTime: 5000,
+        },
+    },
+});
+
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+    throw new Error("Root element not found");
+}
+
+createRoot(rootElement).render(
+    <StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <App />
+            <ReactQueryDevtools />
+        </QueryClientProvider>
+    </StrictMode>
+);
